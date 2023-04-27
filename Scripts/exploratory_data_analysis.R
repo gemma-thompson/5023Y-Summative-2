a00080b4226350d6fa6803cd40d58662fd355786
Gemma <saa21wpu@uea.ac.uk>
2023-04-27 17:33:59 +0000

    making changes to data analysis

---
+++ Scripts/exploratory_data_analysis.R
@@ -0,0 +1,26 @@
+# exploring the data further than just the basic tidying allows us to visualize it
+
+#visualization of the data
+univoltine_butterfly %>%
+  ggplot(aes(x= sex,
+             y= year)) +
+  geom_point(colour="purple")
+
+# the production of this graph highlighted the typos in the sex as males were sometimes spelt incorrectly and females were written singular and plural   
+
+library(tidyverse)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(knitr)
+unique(univoltine_butterfly$sex)
+summary(univoltine_butterfly$sex)
+sum(is.Females(univoltine_butterfly$sex))
+univoltine_butterfly <- univoltine_butterfly %>%
+  mutate_at(c("sex"), ~replace_Females(.,"Female"))
+
+
+
+
+
