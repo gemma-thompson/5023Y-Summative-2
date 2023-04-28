7fb3614c796c7f20864f907422ff248f97da9f44
Gemma <saa21wpu@uea.ac.uk>
2023-04-28 11:34:40 +0000

    made changes to analysing data and data analysis by changing the variables

--- Scripts/exploratory_data_analysis.R
+++ Scripts/exploratory_data_analysis.R
@@ -3,11 +3,11 @@
 #visualization of the data
 univoltine_butterfly %>%
   ggplot(aes(x= sex,
-             y= year)) +
+             y= Year)) +
   geom_point(colour="purple")
 
 # the production of this graph highlighted the typos in the sex as males were sometimes spelt incorrectly and females were written singular and plural   
-
+#code to fix error
 library(tidyverse)
 library(tidyr)
 library(readr)
@@ -17,14 +17,27 @@ library(knitr)
 unique(univoltine_butterfly$sex)
 summary(univoltine_butterfly$sex)
 sum(is.na(univoltine_butterfly$sex))
-univoltine_butterfly <- univoltine_butterfly %>%
-  mutate("Females") 
-  mutate_at(c("sex"), ~replace_Females(.,"Female"))
+
+univoltine_butterfly$sex[univoltine_butterfly$sex=="Females"] <- "Female"
+univoltine_butterfly$sex[univoltine_butterfly$sex=="Maes"] <- "Male"
+univoltine_butterfly$sex[univoltine_butterfly$sex=="Males"] <- "Male"
   
+table(univoltine_butterfly$sex)
+
+#corrected graph displayed
+#visualization of the data
+univoltine_butterfly %>%
+  ggplot(aes(x= sex,
+             y= Year)) +
+  geom_point(colour="purple")
+
 
 # comparing groups
+library(tidyverse)
+library(he)
 univoltine_butterfly %>%
   group_by()
+
 
 
 
