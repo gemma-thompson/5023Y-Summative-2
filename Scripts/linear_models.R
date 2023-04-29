c5b5421752e6a8744ed0c79d5061bdfc70ebe377
Gemma <saa21wpu@uea.ac.uk>
2023-04-29 18:03:07 +0000

    made changes to linear model script

--- Scripts/linear_models.R
+++ Scripts/linear_models.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(GGally)
 library(emmeans)
 library(performance)
+library(emmeans)
 
 lsmodel0 <- lm(formula = rain_jun ~ 1, data = univoltine_butterfly)
 mean(univoltine_butterfly$rain_jun)
@@ -27,5 +28,30 @@ univolutine_butterfly %>%
                size=1.2)+
   theme_bw()
 
+broom::tidy(lsmodel1, conf.int=T)
+GGally::ggcoef_model(lsmodel1,
+                     show_p_values = FALSE,
+                     conf.level = 0.95)
+#
 
+broom::tidy(lsmodel1, conf.int = T, conf.level = 0.99)
+#
+
+means <- emmeans::emmeans(lsmodel1, specs = ~ rain_jun)
+means %>%
+  as_tibble() %>%
+  ggplot(aes(x=rain_jun,
+             y=emmean))+
+  geom_pointrange(aes(
+    ymin=lower.CL,
+    ymax=upper.CL
+  ))
+#
+
+#Assumption checking
+performance::check_model(lsmodel1)
+
+#Normal distribution
+performance::check_model(lsmodel1, check = c("normality", "qq"))
+plot(lsmodel1, which=c(2,2))
 
