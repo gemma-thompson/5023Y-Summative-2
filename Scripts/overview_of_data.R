9ffda09569b16730da3f4069e73a4fe4b1b6c68a
Gemma <saa21wpu@uea.ac.uk>
2023-04-29 16:35:37 +0000

    created overview of data script so that it shows an overview of the plan for r.markdown file and what the data has shown me from analysis and tidying

---
+++ Scripts/overview_of_data.R
@@ -0,0 +1,9 @@
+# The univoltine_butterfly data is looking at different male and female butterflies,
+#and there wing size in the month of June depending on the rainfall, therefore when
+#doing my R.Markdown fie I am going to present data that shows how the rainfall in June
+#affected the males and females wings forewing_length, whether their age had anything to do 
+#with whether the wings were affected more or less, and whether their sex had anything to do with
+#them being affected more or less
+
+#Through the use of different graphs i aim to clearly show whether either sex or age has the most affect on 
+#forewing_length during the rain in June
