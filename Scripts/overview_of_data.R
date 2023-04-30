99833122eaad51ddf5f734658ffc2610635ffb75
Gemma <saa21wpu@uea.ac.uk>
2023-04-30 11:06:26 +0000

    made changes to overview of data script

--- Scripts/overview_of_data.R
+++ Scripts/overview_of_data.R
@@ -1,4 +1,5 @@
-# The univoltine_butterfly data is looking at different male and female butterflies,
+#Introduction on the background of data and plan --------
+#The univoltine_butterfly data is looking at different male and female butterflies,
 #and there wing size in the month of June depending on the rainfall, therefore when
 #doing my R.Markdown fie I am going to present data that shows how the rainfall in June
 #affected the males and females wings forewing_length, whether their age had anything to do 
@@ -10,5 +11,5 @@
 
 #In the linear_models script different statistical analysis tests are performed, i have set out a hypothesis and 
 #a null hypothesis in order to see then which the data supports through these tests. 
-#Hypothesis: 
-#Null Hypothesis: 
+#Hypothesis: Rain will affect the wing length of the butterflies
+#Null Hypothesis: Rain will not affect the wing length of the butterflies 
